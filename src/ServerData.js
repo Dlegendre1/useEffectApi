5b451c860e38fbf7efad9c6a73a0592d889c7b2f
Daniel Legendre <122412456+Dlegendre1@users.noreply.github.com>
2023-10-14 20:20:07 +0000

    Update 4 files

--- src/ServerData.js
+++ src/ServerData.js
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+
 const ServerData = () => {
-  return <h1>No Data To Display</h1>;
+  const [fortniteData, setFortniteData] = useState([]);
+  useEffect(() => {
+    const fortniteFetch = async () => {
+      const res = await fetch("https://fortnite-api.com/v2/news");
+
+      if (res.ok) {
+        const { data } = await res.json();
+        setFortniteData(data.br.motds);
+        return;
+      }
+      setFortniteData([]);
+    };
+    fortniteFetch();
+  }, []);
+
+  return (
+    <div>
+      {fortniteData.map((data) => (
+        <div className="serverContainer" key={data.id}>
+          <h1 className="title">{data.title}</h1>
+          <h2 className="body">{data.body}</h2>
+          <img className="img" src={data.image} alt={data.title} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ServerData;
